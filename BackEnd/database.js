1dacb84238b409b93e99e10ee0f4e58dcd5bc11b
Kareem Land <kland1@students.towson.edu>
2019-11-12 16:16:55 -0500

    Cleaned up payment page and added database method for payment storage

--- BackEnd/database.js
+++ BackEnd/database.js
@@ -9,3 +9,39 @@ MongoClient.connect(url, function(err,db){
     dbProject.createCollection("Pay");
     dbProject.createCollection("maintenence");
 });
+
+function storePayment(){
+    var fname = document.getElementById('fname');
+    var lname = document.getElementById('lname');
+    var street = document.getElementById('stname');
+    var apt = document.getElementById('aptname');
+    var city = document.getElementById('city');
+    var state = document.getElementById('state');
+    var cardName = document.getElementById('Card');
+    var cardNum = document.getElementById('cNumb');
+    var expDate = document.getElementById('eDate');
+    var cvv = document.getElementById('cvv');
+
+    var paymentInfo = {
+        Firstname: fname,
+        LastName: lname,
+        Street: street,
+        ApartmentNum: apt,
+        City: city,
+        State: state,
+        CardName: cardName,
+        CardNum: cardNum,
+        ExpDate: expDate,
+        CVV: cvv
+    }
+    MongoClient.connect(url, function(err,db){
+        if (err) throw err;
+        dbTest.collection("customers").insert(paymentInfo, function(err,res){
+            if (err) throw err;
+            
+            
+        });
+    });
+    alert("Your Payment has been processed");
+    return true;
+}
