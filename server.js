b266578edc28141028be96bd5cde5d6e254bcd54
Myri0d <55466753+Myri0d@users.noreply.github.com>
2019-12-03 17:48:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use("/css",  express.static(path.join(__dirname, 'css')));
 app.use(flash());
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: "bigolsecret,
   resave: false,
   saveUninitialized: false
 }));
