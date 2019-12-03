9522de43ad3c4792200b84633a41c2e392e55613
SheedyWonder <57238673+SheedyWonder@users.noreply.github.com>
2019-12-03 02:08:50 -0500

    Update maintenancedatabase.js

--- BackEnd/maintenancedatabase.js
+++ BackEnd/maintenancedatabase.js
@@ -4,7 +4,25 @@ var url = "mongodb://localhost:3000/";
 MongoClient.connect(url, function(err,db){
     if (err) throw err;
     var dbProject = db.db("projectDB");
-    dbProject.createCollection("names");
-    dbProject.createCollection("phoneNumbers");
-    dbProject.createCollection("passwords");
+    dbProject.createCollection("priority");
+    dbProject.collection("priority").insert(dbProject, function(err,res){
+        if(err) throw err;
+    });
+    dbProject.createCollection("category");
+    dbProject.collection("category").insert(dbProject, function(err,res){
+        if(err) throw err;
+    });
+    dbProject.createCollection("location");
+    dbProject.collection("location").insert(dbProject, function(err,res){
+        if(err) throw err;
+    });
+    dbProject.createCollection("full_description");
+    dbProject.collection("full_description").insert(dbProject, function(err,res){
+        if(err) throw err;
+    });
+    dbProject.createCollection("access_instructions");
+    dbProject.collection("access_instructions").insert(dbProject, function(err,res){
+        if(err) throw err;
+    });
+
 });
