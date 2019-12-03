33c7fd5bb687a585eed4962c1b8c54b2f1a1e6f2
SheedyWonder <57238673+SheedyWonder@users.noreply.github.com>
2019-12-03 16:35:22 -0500

    Update maintenancedatabase.js

--- BackEnd/maintenancedatabase.js
+++ BackEnd/maintenancedatabase.js
@@ -4,29 +4,29 @@ var url = "mongodb://localhost:3000/";
 MongoClient.connect(url, function(err,db){
     if (err) throw err;
     var dbProject = db.db("projectDB");
-    var prority = document.getElementById("ph");
+    var priority = document.getElementById("ph");
     var category = document.getElementById("ph");
     var location = document.getElementById("ph");
     var full_descript = document.getElementById("ph");
     var location = document.getElementById("ph");
     dbProject.createCollection("priority");
-    dbProject.collection("priority").insert(dbProject, function(err,res){
+    dbProject.collection("priority").insert(priority, function(err,res){
         if(err) throw err;
     });
     dbProject.createCollection("category");
-    dbProject.collection("category").insert(dbProject, function(err,res){
+    dbProject.collection("category").insert(category, function(err,res){
         if(err) throw err;
     });
     dbProject.createCollection("location");
-    dbProject.collection("location").insert(dbProject, function(err,res){
+    dbProject.collection("location").insert(location, function(err,res){
         if(err) throw err;
     });
     dbProject.createCollection("full_description");
-    dbProject.collection("full_description").insert(dbProject, function(err,res){
+    dbProject.collection("full_description").insert(full_descript, function(err,res){
         if(err) throw err;
     });
     dbProject.createCollection("access_instructions");
-    dbProject.collection("access_instructions").insert(dbProject, function(err,res){
+    dbProject.collection("access_instructions").insert(location, function(err,res){
         if(err) throw err;
     });
 
