ab48b7cbfec74f8f73dea6486f90f44a2ab6a149
Myri0d <55466753+Myri0d@users.noreply.github.com>
2019-12-02 20:58:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 console.log('Express server started on port %s', server.address().port);
 var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/FrontEnd/non_public_files/Index.html');
+  res.sendfile(__dirname + 'Index.html');
 });
 app.listen(port, function() {
   console.log("Node app is running at localhost:" + app.get('port'))
