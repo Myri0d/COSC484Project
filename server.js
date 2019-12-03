7eabe54a39ec0a79ef00454c8a5e5255281fae54
Myri0d <55466753+Myri0d@users.noreply.github.com>
2019-12-02 21:08:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var server = require('http').Server(app);
 server.listen(8080);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/FrontEnd/non_public_files/Index.html');
+  res.sendfile(__dirname + '/FrontEnd/non_public_files/');
 });
 console.log('Express server started on port %s', server.address().port);
 var port = process.env.PORT || 3000;
